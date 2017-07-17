02da4c7f20f84ffa7b4a29acf8779d36a5d716c3
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-17 16:38:15 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -88,9 +88,9 @@ library(gplots)
     leafletProxy("map", data = zipdata) %>%
       clearShapes() %>%
       addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-        stroke=FALSE, fillOpacity=1, fillColor=pal(classdata)) %>%
+        stroke=FALSE, fillOpacity=0.85, fillColor=pal(classdata)) %>%
       addLegend("bottomleft", values=seq(1,11,1), colors=col2hex(colorlist), title=colorBy,
-        layerId="colorLegend", opacity=0.75, labels=labs)
+        layerId="colorLegend", opacity=0.85, labels=labs)
 	
   })
 

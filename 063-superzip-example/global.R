d4767b5e6b764b475c2efda36f0a293f5bfab7db
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-17 15:39:29 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -6,7 +6,7 @@ allzips<-bigset[1:93,]
 # read in site #, name, location, status
 gauge_data <- read.csv("data/gauge_locations.csv")
 gauge_status <- read.csv("data/gauge_classification.csv")
-allzips$zipcode <- gauge_data$site_no
+allzips$site_no <- gauge_data$site_no
 allzips$statname <- gauge_data$station_nm
 allzips$latitude <- gauge_data$dec_lat_va
 allzips$longitude <- gauge_data$dec_long_v
@@ -18,7 +18,7 @@ redo_full_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", 
 
 redo_full_vol_all <- redo_full_vol[which(redo_full_vol$yeartype == "all"),]
 redo_full_vol_all_April <-redo_full_vol_all[which(redo_full_vol_all$period == "April"),]
-allzips <- merge(allzips, redo_full_vol_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
+allzips <- merge(allzips, redo_full_vol_all_April, by.x="site_no", by.y="gauge", all.y=TRUE)
 
 
 # POST-IMP VOL
@@ -37,9 +37,9 @@ simp_full_intra <- read.csv("data/simp_data_full_vol_90_intraannual_frequency.cs
 simp_imp_intra <- read.csv("data/simp_data_imp_vol_90_intraannual_frequency.csv")
 
 allzips$college <- allzips$college * 100
-allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")
+allzips$site_no <- formatC(allzips$site_no, width=7, format="d", flag="0")
 # try as.char
-row.names(allzips) <- allzips$zipcode
+row.names(allzips) <- allzips$site_no
 
 cleantable <- allzips %>%
   select(

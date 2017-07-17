cdf81907ae0e0107a2453500fc68360822953369
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-17 15:27:33 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -3,23 +3,38 @@ library(dplyr)
 bigset<- readRDS("data/superzip.rds")
 allzips<-bigset[1:93,]
 
-# read in site #, name, location
+# read in site #, name, location, status
 gauge_data <- read.csv("data/gauge_locations.csv")
+gauge_status <- read.csv("data/gauge_classification.csv")
 allzips$zipcode <- gauge_data$site_no
 allzips$statname <- gauge_data$station_nm
 allzips$latitude <- gauge_data$dec_lat_va
 allzips$longitude <- gauge_data$dec_long_v
 
-# subset data 
-# FULL VOL
+# FULL YEAR VOL
 redo_full_vol <- read.csv("data/redo_simp_data_full_vol_90.csv")
-redo_full_vol_all <- subset(redo_full_vol, redo_full_vol$yeartype == "all")
-redo_full_vol_all_April <- subset(redo_full_vol_all, redo_full_vol_all$period == "April")
-allzips$avg <- redo_full_vol_all_April$avg
-allzips$sd <- redo_full_vol_all_April$sd
+redo_full_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
 
-# IMP VOL
+
+redo_full_vol_all <- redo_full_vol[which(redo_full_vol$yeartype == "all"),]
+redo_full_vol_all_April <-redo_full_vol_all[which(redo_full_vol_all$period == "April"),]
+allzips <- merge(allzips, redo_full_vol_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
+
+
+# POST-IMP VOL
 redo_imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
+redo_imp_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+
+# FULL YEAR DUR
+simp_full_dur <- read.csv("data/simp_data_full_vol_90_duration.csv")
+
+# POST IMP DUR
+simp_imp_dur <- read.csv("data/simp_data_full_vol_90_duration.csv")
+
+# FULL YEAR INTRA
+simp_full_intra <- read.csv("data/simp_data_full_vol_90_intraanual_frequency.csv")
+# POST IMP INTRA
+simp_imp_intra <- read.csv("data/simp_data_imp_vol_90_intraanual_frequency.csv")
 
 allzips$college <- allzips$college * 100
 allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")

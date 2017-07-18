b9b5809685eb6d429b9cc884a4e309ca42b25b30
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-18 14:55:36 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -5,10 +5,10 @@ gauge_locations <- read.csv("data/gauge_locations.csv")
 gauge_status <- read.csv("data/gauge_classification.csv")
 gauge_data <- merge(gauge_locations, gauge_status, by.x = "site_no", by.y = "gauge", all.x = TRUE)
 
-allzips$site_no <- gauge_data$site_no
-allzips$statname <- gauge_data$station_nm
-allzips$latitude <- gauge_data$dec_lat_va
-allzips$longitude <- gauge_data$dec_long_v
+#allzips$site_no <- gauge_data$site_no
+#allzips$statname <- gauge_data$station_nm
+#allzips$latitude <- gauge_data$dec_lat_va
+#allzips$longitude <- gauge_data$dec_long_v
 
 # FULL YEARS DATA 
 # MAGNITUDE
@@ -23,7 +23,7 @@ full_intra <- read.csv("data/simp_data_full_vol_90_intraannual_frequency.csv")
 
 full_vol_all <- full_vol[which(full_vol$yeartype == "all"),]
 full_vol_all_April <-full_vol_all[which(redo_full_vol_all$period == "April"),]
-allzips <- merge(allzips, full_vol_all_April, by.x="site_no", by.y="gauge", all.y=TRUE)
+#allzips <- merge(allzips, full_vol_all_April, by.x="site_no", by.y="gauge", all.y=TRUE)
 
 # POST-IMP DATA
 # VOLUME

a12ddf9f1ccb9fe3ce979d25423957b9f438bf5c
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-17 17:39:30 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -63,7 +63,7 @@ function(input, output, session) {
    observe({
     colorBy <- input$metric
     sizeBy <- input$metric
-	if (sizeBy == "magnitude") {
+	if (sizeBy == "avg") {
 	colorlist <-  c("black","orangered","khaki1","olivedrab1","chartreuse3","green4","aquamarine2","deepskyblue4","blue","royalblue4","navyblue")
 	bounds <- c(0,1000,10000,50000,125000,200000,400000,800000,1500000,2500000,3500000)
 	labs <-  c("0","1 AF - 1 TAF","1000 - 10000","10000 - 50000","50000 - 125000","125000 - 200000","200000 - 400000","400000 - 800000","800000 - 1500000","1500000 - 2500000","2500000 - 3500000")

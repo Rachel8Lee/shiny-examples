e3723730650853e02f00548829560a3a878feeda
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-18 15:38:16 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -1,39 +1,42 @@
 library(dplyr)
 
-# read in site #, name, location, status
-gauge_locations <- read.csv("data/gauge_locations.csv")
-gauge_status <- read.csv("data/gauge_classification.csv")
-gauge_data <- merge(gauge_locations, gauge_status, by.x = "site_no", by.y = "gauge", all.x = TRUE)
+bigset<- readRDS("data/superzip.rds")
+allzips<-bigset[1:93,]
 
-allzips$site_no <- gauge_data$site_no
+# read in site #, name, location
+gauge_data <- read.csv("data/gauge_locations.csv")
+gauge_status <- read.csv("data/gauge_classification.csv")
+allzips$zipcode <- gauge_data$site_no
 allzips$statname <- gauge_data$station_nm
 allzips$latitude <- gauge_data$dec_lat_va
 allzips$longitude <- gauge_data$dec_long_v
 
-# FULL YEARS DATA 
-# MAGNITUDE
-full_vol <- read.csv("data/redo_simp_data_full_vol_90.csv")
-full_vol <- merge(full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+# subset data 
+# FULL VOL
+redo_full_vol <- read.csv("data/redo_simp_data_full_vol_90.csv")
+redo_full_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+redo_full_vol_all <- redo_full_vol[which(redo_full_vol$yeartype == "all"),]
+redo_full_vol_all_April <-redo_full_vol_all[which(redo_full_vol_all$period == "April"),]
+allzips <- merge(allzips, redo_full_vol_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
+#allzips$avg <- redo_full_vol_all_April$avg
+#allzips$sd <- redo_full_vol_all_April$sd
 
-# DURATION
-full_dur <- read.csv("data/simp_data_full_vol_90_duration.csv")
+# POST-IMP VOL
+redo_imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 
-# INTRAANNUAL 
-full_intra <- read.csv("data/simp_data_full_vol_90_intraannual_frequency.csv")
+allzips$college <- allzips$college * 100
+allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")
+# try as.char
+row.names(allzips) <- allzips$zipcode
 
-full_vol_all <- full_vol[which(full_vol$yeartype == "all"),]
-full_vol_all_April <-full_vol_all[which(redo_full_vol_all$period == "April"),]
-#allzips <- merge(allzips, full_vol_all_April, by.x="site_no", by.y="gauge", all.y=TRUE)
-
-# POST-IMP DATA
-# VOLUME
-imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
-
-# DURATION
-imp_dur <- read.csv("data/simp_data_full_vol_90_duration.csv")
-
-# INTRA
-imp_intra <- read.csv("data/simp_data_imp_vol_90_intraannual_frequency.csv")
-
-allzips$site_no <- formatC(allzips$site_no, width=7, format="d", flag="0")
-row.names(allzips) <- allzips$site_no
+cleantable <- allzips %>%
+  select(
+    City = city.x,
+    State = state.x,
+    Rank = rank,
+    Score = centile,
+    Superzip = superzip,
+    Population = adultpop,
+    College = college,
+    Income = income
+  )

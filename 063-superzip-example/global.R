5fbc3071b401cf534dbedb94d2824cfc6f15c782
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-19 00:55:44 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -11,19 +11,21 @@ allzips$statname <- gauge_data$station_nm
 allzips$latitude <- gauge_data$dec_lat_va
 allzips$longitude <- gauge_data$dec_long_v
 
+
+# zipdata <- reactive({paste(input$record, input$vars, input$yeartype, input$period, input$sitetype,sep="_")})
 # subset data 
 # FULL VOL
-redo_full_vol <- read.csv("data/redo_simp_data_full_vol_90.csv")
-redo_full_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
-redo_full_vol_all <- redo_full_vol[which(redo_full_vol$yeartype == "all"),]
-redo_full_vol_all_April <-redo_full_vol_all[which(redo_full_vol_all$period == "April"),]
-allzips <- merge(allzips, redo_full_vol_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
+full_avg <- read.csv("data/redo_simp_data_full_vol_90.csv")
+full_avg <- merge(full_avg, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+full_avg_all <- full_avg[which(full_vol$yeartype == "all"),]
+full_avg_all_April <-full_avg_all[which(full_avg_all$period == "April"),]
+allzips <- merge(allzips, full_avg_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
 allzips <- allzips[order(allzips$avg, decreasing = TRUE),]
 #allzips$avg <- redo_full_vol_all_April$avg
 #allzips$sd <- redo_full_vol_all_April$sd
 
 # POST-IMP VOL
-redo_imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
+postimp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 
 allzips$college <- allzips$college * 100
 allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")

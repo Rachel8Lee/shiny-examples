877cb88edab83a928c1dbac6de4d6c8b219f7cfc
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-19 23:15:13 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -1,15 +1,15 @@
 library(dplyr)
 
 bigset<- readRDS("data/superzip.rds")
-allzips<-bigset[1:93,]
+allsites<-bigset[1:93,]
 
 # read in site #, name, location
 gauge_data <- read.csv("data/gauge_locations.csv")
 gauge_status <- read.csv("data/gauge_classification.csv")
-allzips$zipcode <- gauge_data$site_no
-allzips$statname <- gauge_data$station_nm
-allzips$latitude <- gauge_data$dec_lat_va
-allzips$longitude <- gauge_data$dec_long_v
+allsites$zipcode <- gauge_data$site_no
+allsites$statname <- gauge_data$station_nm
+allsites$latitude <- gauge_data$dec_lat_va
+allsites$longitude <- gauge_data$dec_long_v
 
 
 # zipdata <- reactive({paste(input$record, input$vars, input$yeartype, input$period, input$sitetype,sep="_")})
@@ -19,21 +19,19 @@ full_avg <- read.csv("data/redo_simp_data_full_vol_90.csv")
 full_avg <- merge(full_avg, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
 full_avg_all <- full_avg[which(full_avg$yeartype == "all"),]
 full_avg_all_April <-full_avg_all[which(full_avg_all$period == "April"),]
-allzips <- merge(allzips, full_avg_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
-allzips <- allzips[order(allzips$avg, decreasing = TRUE),]
-#allzips$avg <- redo_full_vol_all_April$avg
-#allzips$sd <- redo_full_vol_all_April$sd
+allsites <- merge(allsites, full_avg_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
+allsites <- allsites[order(allsites$avg, decreasing = TRUE),]
+
 
 # POST-IMP VOL
 postimp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 
-allzips$college <- allzips$college * 100
-allzips$zipcode <- formatC(allzips$zipcode, width=7, format="d", flag="0")
+allsites$college <- allsites$college * 100
+allsites$zipcode <- formatC(allsites$zipcode, width=7, format="d", flag="0")
 # try as.char
-row.names(allzips) <- allzips$zipcode
-allzips
+row.names(allsites) <- allsites$zipcode
 
-cleantable <- allzips %>%
+cleantable <- allsites %>%
   select(
     City = city.x,
     State = state.x,

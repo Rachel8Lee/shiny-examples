3bdf1f57c759a7845fac207a1781e58cad2582a1
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-21 12:13:48 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -1,22 +1,16 @@
 library(dplyr)
 
-bigset<- readRDS("data/superzip.rds")
-allsites<-bigset[1:93,]
-
 # read in site #, name, location
-gauge_data <- read.csv("data/gauge_locations.csv")
+gauge_location <- read.csv("data/gauge_locations.csv")
 gauge_status <- read.csv("data/gauge_classification.csv")
-allsites$site_no <- gauge_data$site_no
-allsites$statname <- gauge_data$station_nm
-allsites$latitude <- gauge_data$dec_lat_va
-allsites$longitude <- gauge_data$dec_long_v
+gauge_data <- merge(gauge_location, gauge_status, by.x = "site_no", by.y = "gauge", all.x = TRUE)
 
+allsites <- data.frame(site_no = gauge_data$site_no, station_nm = gauge_data$station_nm, latitude = gauge_data$dec_lat_va, longitude = gauge_data$dec_long_v, status = gauge_data$status)
 
 # zipdata <- reactive({paste(input$record, input$vars, input$yeartype, input$period, input$sitetype,sep="_")})
 # subset data 
 # FULL VOL
 full_avg <- read.csv("data/redo_simp_data_full_vol_90.csv")
-full_avg <- merge(full_avg, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
 full_avg_all <- full_avg[which(full_avg$yeartype == "all"),]
 full_avg_all_April <-full_avg_all[which(full_avg_all$period == "April"),]
 allsites <- merge(allsites, full_avg_all_April, by.x="zipcode", by.y="gauge", all.y=TRUE)
@@ -26,19 +20,4 @@ allsites <- allsites[order(allsites$avg, decreasing = TRUE),]
 # POST-IMP VOL
 postimp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 
-allsites$college <- allsites$college * 100
-allsites$zipcode <- formatC(allsites$zipcode, width=7, format="d", flag="0")
-# try as.char
-row.names(allsites) <- allsites$zipcode
-
-cleantable <- allsites %>%
-  select(
-    City = city.x,
-    State = state.x,
-    Rank = rank,
-    Score = centile,
-    Superzip = superzip,
-    Population = adultpop,
-    College = college,
-    Income = income
-  )
+row.names(allsites) <- allsites$site_no

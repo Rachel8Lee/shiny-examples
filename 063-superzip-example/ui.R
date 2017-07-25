167b39ddd99324d668fea1a581fd2a6ad56c192a
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-25 12:04:04 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -101,7 +101,7 @@ bodies <- dashboardBody(
                    )
             )
             ),
-						fluidRow(title="Download Data Set", column(width=12,
+						fluidRow(column(title="Download Data Set"width=12,
                             box(id="selectbox4",width=NULL, 
 																selectInput("yeartype", "Year Type", year_type)
                             )

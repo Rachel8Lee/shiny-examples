f54f322daef76e2c25c48399a3f305ac25ccb8f5
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-24 22:13:43 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -93,14 +93,7 @@ bodies <- dashboardBody(
 						)
 				),
 
-			tabItem(tabName= "dataexplorer",					
-												fluidRow(column(width=12,
-											box(width=NULL,
-													tags$style(type = "text/css", "#testplot {height: calc(100vh - 410px) !important;}"),
-													plotOutput("testplot")
-											)
-										)
-									),
+			tabItem(tabName= "dataexplorer")
 			)
 		)
 )

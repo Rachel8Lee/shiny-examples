33a19486e4058aaf582fc20e941ecc97e9c03e24
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-25 11:34:51 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -28,93 +28,88 @@ sidebar <- dashboardSidebar(width=150,
 				)
 )
 
-bodies <- dashboardBody( 
-				tags$script(HTML('
-										$(document).ready(function() {
-										$("header").find("nav").append(\'<div class="myClass"> Availability of High-magnitude Streamflow for Groundwater Banking </div>\');
-										})
-										')),
-		tabItems(
-			tabItem(tabName="interactivemap",
-					
-				fluidRow(
-					column(width=6,
-							box(width=NULL, height=NULL,
-							tags$head(
-# Include our custom CSS
-									includeCSS("styles.css"),
-									includeScript("gomap.js")
-							),
-							tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
-							
-							leafletOutput("map")
-							
-							)
-						),
-					
-					#column(width=12,
-						#fluidRow(tags$head(tags$style(HTML('
-						#											.form-group, .selectize-control {
-						#											margin-bottom: 5px;
-						#											}'))), 
-																
-						#			div(
-									column(width=3,
-											box(id="selectbox",width=NULL, #collapsible=TRUE,
-													selectInput("record","Record Length", record_length),
-													selectInput("metric", "Metric", metric)
-												)
-										
-									), 
-										column(width=3,
-											box(id="selectbox2",width=NULL, #collapsible=TRUE,
-													selectInput("period","Time Period", period),
-													selectInput("yeartype", "Year Type", year_type)
-												)
-										
-									), style="font-size:small;")),	 
-								 	
-								 div(width=6,
-									 column(width=6,
-										 box(id="selectbox3", width=NULL,
-												selectInput("site","Site", sites)
-												)
-										)
-									),
-
-							fluidRow(column(width=12,
-											box(id="plotboxinteractive", width=NULL,
-													tags$style(type = "text/css", "#testplot {height: calc(100vh - 410px) !important;}"),
-													plotOutput("testplot")
-											)
-										)
+bodies <- <- dashboardBody( 
+  tags$script(HTML('
+                   $(document).ready(function() {
+                   $("header").find("nav").append(\'<div class="myClass"> Availability of High-magnitude Streamflow for Groundwater Banking </div>\');
+                   })
+                   ')),
+  tabItems(
+    tabItem(tabName="interactivemap",
+            #					fluidRow(column(width=12,
+            #							infoBox(div("Availability of high-magnitude streamflow for groundwater banking in the Central Valley, CA",
+            #											style = "white-space: normal; word-wrap: break-word"), 
+            #									value = NULL,
+            #									subtitle="Interactive Map",
+            #									icon = shiny::icon("globe"), color = "aqua", width = NULL,
+            #									href = NULL, fill = FALSE) 
+            #							)),
+            #					
+            fluidRow(
+              column(width=6,
+                     box(width=NULL, height=NULL,
+                         tags$head(
+                           # Include our custom CSS
+                           includeCSS("styles.css"),
+                           includeScript("gomap.js")
+                         ),
+                         tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
+                         
+                         leafletOutput("map")
+                         
+                     )
+              ),
+              column(width=6,
+                     fluidRow(tags$head(tags$style(HTML('
+                                                        .form-group, .selectize-control {
+                                                        margin-bottom: 5px;
+                                                        }'))), 
+									div(
+									  column(width=12,
+									         box(id="selectbox",width=NULL, #collapsible=TRUE,
+									             #													selectInput("sites", "Site Type", site_type),
+									             selectInput("metric", "Metric", vars),
+									             selectInput("record","Record Length", record_length),
+									             selectInput("period","Time Period", period),
+									             selectInput("site","Site Selection", sites)
+									             #													selectInput("yeartype", "Year Type", yeartype)
+									         )
+									         
+									  ), style="font-size:small;")),
+									fluidRow(column(width=12,
+									                box(width=NULL,
+									                    tags$style(type = "text/css", "#testplot {height: calc(100vh - 410px) !important;}"),
+									                    plotOutput("testplot")
+									                )
 									)
-							)
-						#)
-				),
+									)
+                     )
+              )
+    ),
+    tabItem(tabName= "dataexplorer",
+            fluidRow(column(width=4,
+                            box(id="selectbox",width=NULL, #collapsible=TRUE,
+                                selectInput("record","Record Length", record_length),
+                                selectInput("metric", "Metric", metric),
+                                selectInput("period","Time Period", period),
+                                selectInput("site1","Site", sites),
+                                selectInput("site2","Site", sites),
+                                selectInput("site3","Site", sites)
+                            )
+            ),
+            #),
+            
+            #fluidRow(
+            column(width=8,
+                   box(id="plotboxexplorer", width=NULL,
+                       tags$style(type = "text/css", "#testplot {height: calc(100vh - 410px) !important;}"),
+                       plotOutput("testplot2")
+                   )
+            )
+            )
+    )))
 
-			tabItem(tabName= "dataexplorer",
-			fluidRow(column(width=4,
-											box(id="selectbox",width=NULL, #collapsible=TRUE,
-													selectInput("record","Record Length", record_length),
-													selectInput("metric", "Metric", metric),
-													selectInput("period","Time Period", period),
-													selectInput("site1","Site", sites),
-													selectInput("site2","Site", sites),
-													selectInput("site3","Site", sites)
-												)
-										),
-									#),
-							
-			#fluidRow(
-				column(width=8,
-				       box(id="plotboxexplorer", width=NULL,
-					         tags$style(type = "text/css", "#testplot {height: calc(100vh - 410px) !important;}"),
-						                  plotOutput("testplot2")
-						      	         )
-					         )
-			        )
-		)))
+
 
 
 dashboardPage(

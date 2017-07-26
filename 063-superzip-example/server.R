b993efcf53e44b7f0d42a23ae19d1e8f08d91334
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-25 23:59:24 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -23,15 +23,6 @@ function(input, output, session) {
       ) %>%
       setView(lng = -120.51, lat = 38.06, zoom = 6)
   })
-	
-tags$style(type = "text/css", "html, body {width:100%;height:100%}",
-".leaflet .legend i{
-border-radius: 50%;
-width: 10px;
-height: 10px;
-margin-top: 4px;
-}"
-),
  
   # A reactive expression that returns the set of zips that are
   # in bounds right now

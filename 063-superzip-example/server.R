88034ffb5e60c05dd2cc5916da325fef1db63a85
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-26 18:53:21 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -61,7 +61,7 @@ function(input, output, session) {
   	sizes <- c(1,3,6,9,12,15,18,21,24,27,30)}
 	else if (input$map_zoom == 7){
 	sizes <- c(1,4,7,10,13,16,19,22,25,28,31)}
-	else if (input$map_zoom == 8) {
+	else {
   	sizes <- c(2,5,8,11,14,17,20,23,26,29,32)}
   }
   

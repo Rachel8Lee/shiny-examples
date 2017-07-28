82547ee9641e2351e9aa46dfe9796274e75f9613
Rachel8Lee <rchlee@ucdavis.edu>
2017-07-28 16:10:06 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -12,7 +12,7 @@ allsites <- data.frame(site_no = gauge_data$site_no, station_nm = gauge_data$sta
 # FULL VOL
 full_magnitude <- read.csv("data/redo_simp_data_full_vol_90.csv")
 full_magnitude_all <- full_magnitude[which(full_magnitude$yeartype == "all"),]
-full_magnitude_all_apr <-full_magnitude_all[which(full_magnitude_all$period == "April"),]
+full_magnitude_all_apr <-full_magnitude_all[which(full_magnitude_all$period == "January"),]
 
 imp_magnitude <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 imp_magnitude_all <- imp_magnitude[which(imp_magnitude$yeartype == "all"),]

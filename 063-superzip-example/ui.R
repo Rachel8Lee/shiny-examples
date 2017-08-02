975a0dfae9d8720681a6a9421c2180ac056da591
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-02 15:39:38 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -73,7 +73,7 @@ bodies <- dashboardBody(
 									column(width=4,
 											box(id="selectbox",width=NULL, 
 													 checkboxGroupInput("sitetype", "Select Site Type:", c("Impaired" = "impaired", "Unimpaired" = "unimpaired"),
-																						 selected = c("Impaired", "Unimpaired"))
+																						 selected = c("impaired", "unimpaired"))
 												)
 										
 									), style="font-size:small;")),

1f6d2cca739d8d8ee97d117c3d11da5543b56fd0
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-03 22:05:03 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -83,7 +83,7 @@ function(input, output, session) {
   observe({
 		# checkbox for site types
     if (length(input$sitetype) == 1) {
-		  sitedata <- sitedata[which(sitedata[5] == "impaired")]
+		  sitedata <- sitedata[which(sitedata[5] == input$sitetype),]
 		}
 		
 	##add if statement for  metric variables

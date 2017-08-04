d3fe22a5d68762068dfb87760b9a4dd6793a4919
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-03 22:51:04 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -7,7 +7,54 @@ gauge_data <- merge(gauge_location, gauge_status, by.x = "site_no", by.y = "gaug
 
 allsites <- data.frame(site_no = gauge_data$site_no, station_nm = gauge_data$station_nm, latitude = gauge_data$dec_lat_va, longitude = gauge_data$dec_long_v, status = gauge_data$status)
 
-# zipdata <- reactive({paste(input$record, input$vars, input$yeartype, input$period, input$sitetype,sep="_")})
+# for barplots
+gauge_data <- read.csv("data/gauge_locations.csv")
+gauge_data <- gauge_data[,2:length(gauge_data)]
+gauge_status <- read.csv("data/gauge_classification.csv")
+gauge_status<- gauge_status[,2:length(gauge_status)]
+
+# subset data 
+# FULL VOL
+redo_full_vol <- read.csv("data/redo_simp_data_full_vol_90.csv")
+redo_full_vol <- redo_full_vol[,2:length(redo_full_vol)]
+redo_full_vol <- merge(redo_full_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+redo_full_dur <- read.csv("data/simp_data_full_vol_90_duration.csv")
+redo_full_dur <- redo_full_dur[,2:length(redo_full_dur)]
+redo_full_dur <- merge(redo_full_dur, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+redo_full_nmpks <- read.csv("data/simp_data_full_vol_90_intraannual_frequency.csv")
+redo_full_nmpks <- redo_full_nmpks[,2:length(redo_full_nmpks)]
+redo_full_nmpks <- merge(redo_full_nmpks, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+
+redo_imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
+redo_imp_vol <- redo_imp_vol[,2:length(redo_imp_vol)]
+redo_imp_vol <- merge(redo_imp_vol, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+redo_imp_dur <- read.csv("data/simp_data_imp_vol_90_duration.csv")
+redo_imp_dur <- redo_imp_dur[,2:length(redo_imp_dur)]
+redo_imp_dur <- merge(redo_imp_dur, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+redo_imp_nmpks <- read.csv("data/simp_data_imp_vol_90_intraannual_frequency.csv")
+redo_imp_nmpks <- redo_imp_nmpks[,2:length(redo_imp_nmpks)]
+redo_imp_nmpks <- merge(redo_imp_nmpks, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+
+
+vol.frame <- redo_full_vol
+vol.frame[is.na(vol.frame)] <- 0 
+dur.frame<- redo_full_dur
+dur.frame[is.na(dur.frame)] <- 0 
+nmpks.frame<- redo_full_nmpks
+nmpks.frame[is.na(nmpks.frame)] <- 0 
+imp.vol.frame<- redo_imp_vol
+imp.vol.frame[is.na(imp.vol.frame)] <- 0 
+imp.dur.frame<- redo_imp_dur
+imp.dur.frame[is.na(imp.dur.frame)] <- 0 
+imp.nmpks.frame<- redo_imp_nmpks
+imp.nmpks.frame[is.na(imp.nmpks.frame)] <- 0 
+
+full <- rbind.data.frame(dur.frame, vol.frame, nmpks.frame)
+full["tag"] <- "full" 
+post <- rbind.data.frame(imp.dur.frame, imp.vol.frame, imp.nmpks.frame)
+post["tag"] <- "post-impairment"
+allsites <- rbind.data.frame(full, post)
+
 # subset data 
 # FULL VOL
 full_magnitude <- read.csv("data/redo_simp_data_full_vol_90.csv")
@@ -26,9 +73,9 @@ imp_dur <- read.csv("data/simp_data_imp_vol_90_duration.csv")
 imp_dur_all <- imp_dur[which(imp_dur$yeartype == "all"),]
 imp_dur_all_apr <- imp_dur_all[which(imp_dur_all$period == "April"),]
 
-allsites <- merge(allsites, full_dur_all_apr, by.x="site_no", by.y="gauge", all.y=TRUE)
-allsites <- allsites[order(allsites$avg, decreasing = TRUE),]
-row.names(allsites) <- allsites$site_no
+#allsites <- merge(allsites, full_dur_all_apr, by.x="site_no", by.y="gauge", all.y=TRUE)
+#allsites <- allsites[order(allsites$avg, decreasing = TRUE),]
+#row.names(allsites) <- allsites$site_no
 
 # TODO: Add comment
 # 

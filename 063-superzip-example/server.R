de08859da7d825a2a42c9c1b1e0aa8581e8c735c
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-03 19:51:23 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -89,7 +89,7 @@ function(input, output, session) {
 	    labs <-  c("0","1 AF - 1 TAF","1TAF - 10TAF","10TAF- 50TAF","50TAF - 125TAF","125TAF - 200TAF","200TAF - 400TAF","400TAF - 800TAF","800TAF - 1.5MAF","1.5MAF - 2.5MAF","2.5MAF - 3.5MAF")
 	    legendTitle <- "Magnitude (HMF Volume)"
 	    zoomsize <- input$map_zoom
-      sizes <- c(1,3,6,9,12,15,18,21,24,27,30) 
+            sizes <- c(1,3,6,9,12,15,18,21,24,27,30) 
 	    scalar <- 10000/sitedata$avg[1]
 	    rad <- scalar*sitedata$avg + 3000
 	  }
@@ -97,18 +97,18 @@ function(input, output, session) {
     else if (input$metric == "duration") {
 	    colorlist <- c("black", "maroon", "magenta", "darkslateblue", "royalblue", "turquoise")
 	    bounds <- c(0,1,10,20,40,60,80)
-	    labs <-	c("0","1 - 10","10 - 20","20 - 40","40 - 60", "60 - 80")
+	    labs <- c("0","1 - 10","10 - 20","20 - 40","40 - 60", "60 - 80")
 	    legendTitle <- "Duration (HMF Days)"
-      sizes <- c(15,18,21,24,27,30) 
-			scalar <- 10000/sitedata$avg[1]
+            sizes <- c(15,18,21,24,27,30) 
+	    scalar <- 10000/sitedata$avg[1]
 	    rad <- scalar*sitedata$avg + 3000
 	  }
 		
 	  else { 	
-      colorlist <- c("black","yellow","darkorange","deeppink","darkviolet","navy")
+            colorlist <- c("black","yellow","darkorange","deeppink","darkviolet","navy")
 	    bounds <- c(0,1,4,8,12,16,20)
 	    labs <- c("0", "1 - 4","4 - 8", "8 - 12", "12 - 16","16 - 20")
-      legendTitle <- "No. 1-Day Peaks"
+            legendTitle <- "No. 1-Day Peaks"
 	    sizes <- c(15,18,21,24,27,30) 
 	    rad <- 3000
 	  }  
@@ -118,14 +118,15 @@ function(input, output, session) {
     dom <- seq(1,length(bounds),1)  
 	 
     colorData <- sitedata$avg
-	  classdata <- rep(NA,length(colorData))
-	  classdata[which(colorData== bounds[[1]])] <- 1
-	  for(i in 2:length(bounds)){
-		  classdata[which(colorData > bounds[[i-1]] & colorData <= bounds[[i]] )] <- i
-	  }
+    classdata <- rep(NA,length(colorData))
+    classdata[which(colorData == bounds[[1]])] <- 1
+    classdata[which(colorData == "NA")] <- 1  
+    for(i in 2:length(bounds)){
+      classdata[which(colorData > bounds[[i-1]] & colorData <= bounds[[i]] )] <- i
+    }
      
     pal <- colorFactor(palette=colorlist, domain=dom, na.color="black")
-	  colorlist <- col2hex(colorlist)
+    colorlist <- col2hex(colorlist)
     # modify legend size 
     colorAdditions <- paste0(colorlist, "; width:", sizes, "px; height:", sizes, "px")
     labelAdditions <- paste0("<div style='display: inline-block;height: ", sizes, "px;margin-top: 4px;line-height: ", sizes, "px;'>", labs, "</div>")

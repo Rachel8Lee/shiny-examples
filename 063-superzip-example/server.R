0d15c849c95749eea02b1402c44aee4f86776d13
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-07 12:57:37 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -105,7 +105,7 @@ function(input, output, session) {
 	    bounds <- c(0,1000,10000,50000,125000,200000,400000,800000,1500000,2500000,3500000)
 	    labs <-  c("0","1 AF - 1 TAF","1TAF - 10TAF","10TAF- 50TAF","50TAF - 125TAF","125TAF - 200TAF","200TAF - 400TAF","400TAF - 800TAF","800TAF - 1.5MAF","1.5MAF - 2.5MAF","2.5MAF - 3.5MAF")
 	    legendTitle <- "Magnitude (HMF Volume)"
-	    zoomsize <- inpu$map_zoom
+	    zoomsize <- input$map_zoom
       sizes <- c(1,3,6,9,12,15,18,21,24,27,30) 
 	    rad <- usedata$avg/120 + 3000
 	  }

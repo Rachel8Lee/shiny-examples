aeddd633290ab5b78dacab9d36a881c6989103fd
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-09 15:22:42 -0700

    Delete convert.js

--- 063-superzip-example/convert.js
+++
@@ -1,9 +0,0 @@
-var metersPerPixel = function(latitude, zoomLevel) {
-  var earthCircumference = 40075017;
-  var latitudeRadians = latitude * (Math.PI/180);
-  return earthCircumference * Math.cos(latitudeRadians) / Math.pow(2, zoomLevel + 8);
-};
-
-var pixelValue = function(latitude, meters, zoomLevel) {
-  return meters / metersPerPixel(latitude, zoomLevel);
-};

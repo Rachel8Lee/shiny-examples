edf1bf37007658a91e4becf1a277b5c2cd0c1acf
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-10 16:40:21 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -111,7 +111,7 @@ bodies <- dashboardBody(
                                 selectInput("periodDE","Time Period", period)),
                                 selectInput("site1","Site", sites),
                                 selectInput("site2","Site", siteblank, selected = " "),
-                                selectInput("site3","Site", siteblank, selected = " "))))
+                                selectInput("site3","Site", siteblank, selected = " ")))
 
 						),
 						fluidRow(				 

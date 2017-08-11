60d84fd390f7f43a539147fd2fa89f007e0e40f7
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-10 21:18:59 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -16,6 +16,10 @@ redo_full_dur <- merge(redo_full_dur, gauge_status, by.x="gauge", by.y="gauge", 
 redo_full_nmpks <- read.csv("data/simp_data_full_vol_90_intraannual_frequency.csv")
 redo_full_nmpks <- redo_full_nmpks[,2:length(redo_full_nmpks)]
 redo_full_nmpks <- merge(redo_full_nmpks, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+fracywf.full <- read.csv("data/simp_data_full_vol_90_intERannual_frequency.csv")
+fracywf.full[is.na(fracywf.full)] <- 0
+fracywf.full$X <- NULL
+fracywf.full <-  merge(fracywf.full, gauge_data, by.x="gauge", by.y="site_no", all.x=TRUE)
 fracywfFG <- fracywf.full[c(1,2,3,5,6,8,13,14)]
 colnames(fracywfFG)[4]<- "avg"
 colnames(fracywfFG)[5]<- "sd"
@@ -30,6 +34,10 @@ redo_imp_dur <- merge(redo_imp_dur, gauge_status, by.x="gauge", by.y="gauge", al
 redo_imp_nmpks <- read.csv("data/simp_data_imp_vol_90_intraannual_frequency.csv")
 redo_imp_nmpks <- redo_imp_nmpks[,2:length(redo_imp_nmpks)]
 redo_imp_nmpks <- merge(redo_imp_nmpks, gauge_status, by.x="gauge", by.y="gauge", all.x=TRUE)
+fracywf.imp <- read.csv("data/simp_data_imp_vol_90_intERannual_frequency.csv")
+fracywf.imp[is.na(fracywf.imp)] <- 0
+fracywf.imp <-  merge(fracywf.imp, gauge_data, by.x="gauge", by.y="site_no", all.x=TRUE)
+fracywf.imp$X <- NULL
 fracywfIG <- fracywf.imp[c(1,2,3,5,6,8,13,14)]
 colnames(fracywfIG)[4]<- "avg"
 colnames(fracywfIG)[5]<- "sd"

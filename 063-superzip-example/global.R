a2cea6e9c9140eee944c360ae5e8ebb93f5f6d0a
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-11 21:57:53 -0700

    Update global.R

--- 063-superzip-example/global.R
+++ 063-superzip-example/global.R
@@ -27,6 +27,21 @@ fracywfFG <- fracywf.full[c(1,2,3,5,6,8,13,14)]
 colnames(fracywfFG)[4]<- "avg"
 colnames(fracywfFG)[5]<- "sd"
 fracywfFG$sd <-"NA"
+timing.fullcsv <- read.csv("data/redo_COM90_mag_date_sd_full.csv")
+timing.full.all <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$avg_DOHY, sd=timing.fullcsv$sd_DOHY, yeartype="All")
+timing.full.C <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$C_avg, sd=timing.fullcsv$C_sd, yeartype="C")
+timing.full.D <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$D_avg, sd=timing.fullcsv$D_sd, yeartype="D")
+timing.full.BN <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$BN_avg, sd=timing.fullcsv$BN_sd, yeartype="BN")
+timing.full.AN <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$AN_avg, sd=timing.fullcsv$AN_sd, yeartype="AN")
+timing.full.W <- data.frame(gauge=timing.fullcsv$gauge, avg=timing.fullcsv$W_avg, sd=timing.fullcsv$W_sd, yeartype="W")
+timing.full.blanks <- data.frame(gauge=timing.fullcsv$gauge, avg=NA, sd=NA, yeartype=" ")
+timing.full <- rbind.data.frame(timing.full.all,timing.full.C,timing.full.D,
+		timing.full.BN,timing.full.AN,timing.full.W, timing.full.blanks)
+timing.full <-  merge(timing.full, gauge_data, by.x="gauge", by.y="site_no", all.x=TRUE)
+timing.full$X <- NULL
+timing.full$period <"NA"
+timing.full$valtype <- "timing"
+time.frame <- timing.full[c(1,4,11,2,3,12,9,10)
 
 redo_imp_vol <- read.csv("data/redo_simp_data_imp_vol_90.csv")
 redo_imp_vol <- redo_imp_vol[,2:length(redo_imp_vol)]
@@ -45,6 +60,24 @@ fracywfIG <- fracywf.imp[c(1,2,3,5,6,8,13,14)]
 colnames(fracywfIG)[4]<- "avg"
 colnames(fracywfIG)[5]<- "sd"
 fracywfIG$sd <-"NA"
+timing.impcsv <- read.csv("data/redo_COM90_mag_sd_date_imp_updated.csv")
+timing.imp.all <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$avg_DOHY, sd=timing.impcsv$sd_DOHY, yeartype="All")
+timing.imp.C <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$C_avg, sd=timing.impcsv$C_sd, yeartype="C")
+timing.imp.D <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$D_avg, sd=timing.impcsv$D_sd, yeartype="D")
+timing.imp.BN <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$BN_avg, sd=timing.impcsv$BN_sd, yeartype="BN")
+timing.imp.AN <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$AN_avg, sd=timing.impcsv$AN_sd, yeartype="AN")
+timing.imp.W <- data.frame(gauge=timing.impcsv$gauge, avg=timing.impcsv$W_avg, sd=timing.impcsv$W_sd, yeartype="W")
+timing.imp.blanks <- data.frame(gauge=timing.impcsv$gauge, avg=NA, sd=NA, yeartype=" ")
+timing.imp <- rbind.data.frame(timing.imp.all,timing.imp.C,timing.imp.D,
+		timing.imp.BN,timing.imp.AN,timing.imp.W, timing.imp.blanks)
+timing.imp <-  merge(timing.imp, gauge_data, by.x="gauge", by.y="site_no", all.x=TRUE)
+timing.imp$X <- NULL
+timing.imp.blanks <- data.frame(gauge=timing.impcsv$gauge, avg=NA, sd=NA, yeartype=" ", agency_cd=timing.imp$agency_cd,
+		station_nm=timing.imp$station_nm,dec_lat_va=timing.imp$dec_lat_va, dec_long_v=timing.imp$dec_long_v, 
+		basin=timing.imp$basin, status=timing.imp$status)
+timing.imp$period <"NA"
+timing.imp$valtype <- "timing"
+imp.time.frame <- timing.imp[c(1,4,11,2,3,12,9,10)
 
 vol.frame <- redo_full_vol
 vol.frame[is.na(vol.frame)] <- 0 
@@ -60,9 +93,9 @@ imp.dur.frame[is.na(imp.dur.frame)] <- 0
 imp.nmpks.frame<- redo_imp_nmpks
 imp.nmpks.frame[is.na(imp.nmpks.frame)] <- 0 
 
-fullG <- rbind.data.frame(dur.frame, vol.frame, nmpks.frame, fracywfFG)
+fullG <- rbind.data.frame(dur.frame, vol.frame, nmpks.frame, fracywfFG, time.frame)
 fullG["tag"] <- "full" 
-postG <- rbind.data.frame(imp.dur.frame, imp.vol.frame, imp.nmpks.frame, fracywfIG)
+postG <- rbind.data.frame(imp.dur.frame, imp.vol.frame, imp.nmpks.frame, fracywfIG, imp.time.frame)
 postG["tag"] <- "post-impairment"
 alldatG <- rbind.data.frame(fullG, postG)
 allsites <- merge(alldatG, gauge_location, by.x = "gauge", by.y = "site_no", all.x = TRUE)

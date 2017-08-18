db59c6160e558e3652438685545052c837c97e8e
Rachel8Lee <rchlee@ucdavis.edu>
2017-08-17 18:27:32 -0700

    Update ui.R

--- 063-superzip-example/ui.R
+++ 063-superzip-example/ui.R
@@ -83,7 +83,7 @@ bodies <- dashboardBody(
 									)),
 									fluidRow(column(width=12,
 									                box(width=NULL,
-									                    #tags$style(type = "text/css", "#IMplot height:{20000px} !important;}"),
+									                    #tags$style(type = "text/css", "#IMplot height:{15000px} !important;}"),
 									                    plotOutput("IMplot", height = "10000px")
 									                )
 									)

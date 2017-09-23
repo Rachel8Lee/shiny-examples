455b47f3b9e42b203bf880f2c4ae076d57774e9c
Rachel8Lee <rchlee@ucdavis.edu>
2017-09-22 18:19:30 -0700

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -30,23 +30,8 @@ function(input, output, session) {
       ) %>%
       addScaleBar("bottomright",  options = scaleBarOptions(imperial = FALSE)) %>%
       setView(lng = -120.51, lat = 38.06, zoom = 6)
-		  area <- readShapePoly("basin.shp")
-      colors <- brewer.pal(9, "BuGn")
-	    mapImage <- get_map(location = c(lon = -118, lat = 37.5),
-        color = "color",
-        source = "osm",
-        zoom = 6)
-	    area.points <- fortify(area)
-	    ggmap(mapImage) +
-      geom_polygon(aes(x = long,
-        y = lat,
-        group = group),
-     data = area.points,
-     color = colors[9],
-     fill = colors[6],
-     alpha = 0.5) +
-  labs(x = "Longitude",
-  y = "Latitude")
+      area <- readShapePoly("basin.shp")
+      plot(area)
   })
 	
   # reactive data set 
